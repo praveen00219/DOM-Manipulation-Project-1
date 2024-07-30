057fb9086b43742ac5f1231029cdaed299cb42b2
praveen00219 <paru2192000@gmail.com>
2024-07-30 13:30:44 +0530

    created DOM Project-1(DOM Manipulation Example)

---
+++ script.js
@@ -0,0 +1,23 @@
+// Use getElementById to fetch the target div element
+var divElement = document.getElementById("myDiv");
+
+// Change the background color of the fetched div element to yellow
+divElement.style.backgroundColor = "yellow";
+
+// Add a margin of 20 pixels to the element
+divElement.style.margin = "20px";
+
+// Apply a padding of 10 pixels
+divElement.style.padding = "10px";
+
+// Change the font size to 18 pixels
+divElement.style.fontSize = "18px";
+
+// Set the font weight to bold
+divElement.style.fontWeight = "bold";
+
+// Change the height of the element to 200 pixels
+divElement.style.height = "200px";
+
+// Modify the width of the element to 300 pixels
+divElement.style.width = "300px";
